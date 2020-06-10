9ea34806e885d16babd7e73cca033bf73fb5f52f
mudassarnasar <mudassarnazar826@gmail.com>
2020-06-10 17:25:57 +0530

    textbox issue

--- js/calculate.js
+++ js/calculate.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-
-    var selected = new Array();
+    
     $('#nav-total-tab').click(function(e) {
         $('input[type="checkbox"]').each(function(){
             if($(this).prop("checked") == true){
@@ -15,28 +14,5 @@ $(document).ready(function(){
 
 
     });
-
-
-
-
-
-    function calculateSum() {
-        var sum = 0;
-    $('input[type="checkbox"]').click(function(){
-        if($(this).prop("checked") == true){
-            var price = $('input[type="text"]').val();
-            console.log(price);
-
-
-            
-
-            console.log("Checkbox is checked.");
-        }
-        else if($(this).prop("checked") == false){
-            console.log("Checkbox is unchecked.");
-        }
-    });
-
-    }
-    
+ 
 });
