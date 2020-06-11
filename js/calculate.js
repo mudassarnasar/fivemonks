d5aaeb56fd51e3e4908addcb57a3884e1cc51b5f
Abulkhair <abulkhair.shaikh93@gmail.com>
2020-06-11 23:09:32 +0530

    added calcuation logic

--- js/calculate.js
+++ js/calculate.js
@@ -1,61 +1,37 @@
-$(document).ready(function(){
-    
-    $('#nav-total-tab').click(function(e) {
-        $('input[type="checkbox"]').each(function(){
-            if($(this).prop("checked") == true){
-                var price = $('input[type="text"]').val();
-                console.log(price);
-                console.log("Checkbox is checked.");
-            }
-            else if($(this).prop("checked") == false){
-                console.log("Checkbox is unchecked.");
-            }
-        });
+$(document).ready(function () {
+
+   function calculateSum() {
+
+      var sumTotal = 0;
+      $('table tbody tr').each(function () {
+         var $tr = $(this);
 
 
-    });
+         if ($tr.find('input[type="checkbox"]').is(':checked')) {
 
 
-    function calculateSum(){
-        alert('functoin called');
-        var sumTotal=0;
-           $('table tbody tr').each(function() {
-             var $tr = $(this);
-       
-             if ($tr.find('input[type="checkbox"]').is(':checked')) {
-                 
-               var $columns = $tr.find('td').next('td').next('td');
-                
-                var $Qnty=parseInt($tr.find('input[type="text"]').val());
-        var $Cost=parseInt($columns.next('td').html().split('$')[1]);
-                sumTotal+=$Qnty*$Cost;
-             }
-           });
-       
-              $("#totalbill").val(sumTotal);
-              
-       }
-       
-        //  $('#sum').on('click', function() {
-            
-        //    calculateSum();
-        //  });
-       
-        //  $("input[type='text']").keyup(function() {
-        //     calculateSum();
-       
-        //  });
-         
-        //   $("input[type='checkbox']").change(function() {
-        //     calculateSum();
-       
-        //  });
+            var $columns = $tr.find('td').next('td').next('td');
+
+            var $Qnty = parseInt($tr.find('input[type="text"]').val());
+
+            var $Cost = parseInt($columns.next('td').html());
+
+            sumTotal += $Qnty * $Cost;
+         }
+      });
+
+      $("#price").html(sumTotal);
+
+   }
+
+   $("#nav-total-tab").click(function () {
+
+
+      calculateSum();
+
+   });
 
 
 
 
-
-
-
- 
 });
